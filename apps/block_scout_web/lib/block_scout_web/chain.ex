8be5c1b7d1ba8eb5e5bde00227507133d5e27de8
POA <33550681+poa@users.noreply.github.com>
2023-02-05 19:31:51 +0300

    Add API v2 for Optimism Output Roots

--- apps/block_scout_web/lib/block_scout_web/chain.ex
+++ apps/block_scout_web/lib/block_scout_web/chain.ex
@@ -24,6 +24,7 @@ defmodule BlockScoutWeb.Chain do
     Block,
     InternalTransaction,
     Log,
+    OptimismOutputRoot,
     SmartContract,
     Token,
     Token.Instance,
@@ -411,6 +412,10 @@ defmodule BlockScoutWeb.Chain do
 
   defp paging_params(%SmartContract{} = smart_contract) do
     %{"smart_contract_id" => smart_contract.id}
+  end
+
+  defp paging_params(%OptimismOutputRoot{l2_output_index: index}) do
+    %{"index" => index}
   end
 
   # clause for search results pagination
